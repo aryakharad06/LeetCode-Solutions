ee213a2ecdef8b1112fb2051e17d4644205077b0
aryakharad06 <aryakharad06@gmail.com>
2026-09-08 10:34:57 +0530

    Time: 0 ms (100%), Space: 19.3 MB (77.6%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -8,11 +8,11 @@ class Solution:
             if nums1[i] >= nums2[j]:
                 nums1[k] = nums1[i]
                 i -= 1
+                k -= 1
             else:
                 nums1[k] = nums2[j]
                 j -= 1
-
-            k -= 1
+                k -= 1
 
         while j >= 0:
             nums1[k] = nums2[j]
